9091bdbb2b8d1da21f59f1800da0cb3ff3d058a9
Dheeraj Mahra <dheerajmahra873@gmail.com>
2023-08-17 20:50:58 +0530

    fix: fix a false positive test

--- src/__tests__/Toast.test.js
+++ src/__tests__/Toast.test.js
@@ -8,7 +8,7 @@ afterEach(() => {
 
 test('Display success toast on `Trigger Success Toast` button click', async () => {
   const { user } = getExampleDom();
-  user.click(screen.getByRole("button", { name: /Trigger success toast/i }));
+  await user.click(screen.getByRole("button", { name: /Trigger success toast/i }));
   const successToast = await screen.findByRole("alert", { name: /spawned a success toast/i });
 
   expect(successToast).toBeInTheDocument();
@@ -17,7 +17,7 @@ test('Display success toast on `Trigger Success Toast` button click', async () =
 
 test('Display error toast on `Trigger Error Toast` button click', async () => {
   const { user } = getExampleDom();
-  user.click(screen.getByRole("button", { name: /Trigger error toast/i }));
+  await user.click(screen.getByRole("button", { name: /Trigger error toast/i }));
   const errorToast = await screen.findByRole("alert", { name: /spawned an error toast/i });
 
   expect(errorToast).toBeInTheDocument();
@@ -29,22 +29,18 @@ test('\`n\` toasts appear on \`n\` button clicks', async () => {
   const succesBtn = screen.getByRole("button", { name: /Trigger success toast/i });
 
   for(let i = 0; i < 3; i++) {
-    user.click(succesBtn);
+    await user.click(succesBtn);
   }
   
   await waitFor(() => {
     expect(screen.getAllByRole("alert")).toHaveLength(3);
-    /**
-     * Below test also passes unexpectedly ???
-     */
-    //expect(screen.getAllByRole("alert")).toHaveLength(2);
   });
 });
 
 
 test('`x` icon hidden for non-closable toast', async () => {
   const { user } = getExampleDom({ closable: false });
-  user.click(screen.getByRole("button", { name: /Trigger success toast/i }));
+  await user.click(screen.getByRole("button", { name: /Trigger success toast/i }));
 
   const successToast = await screen.findByRole("alert", { name: /spawned a success toast/i });
   expect(successToast.querySelector('.toast-dismiss')).not.toBeInTheDocument();
@@ -52,10 +48,10 @@ test('`x` icon hidden for non-closable toast', async () => {
 
 test('`x` click closes the Toast', async () => {
   const { user } = getExampleDom();
-  user.click(screen.getByRole("button", { name: /Trigger success toast/i }));
+  await user.click(screen.getByRole("button", { name: /Trigger success toast/i }));
 
   const successToast = await screen.findByRole("alert", { name: /spawned a success toast/i });
-  user.click(successToast.querySelector('.toast-dismiss'));
+  await user.click(successToast.querySelector('.toast-dismiss'));
 
   await waitForElementToBeRemoved(() => screen.queryByRole("alert"));
 });
